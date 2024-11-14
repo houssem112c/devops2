ff035114fe060cd800e245039a3eabeea8504581
achrefmaddouri <76907474+achrefmaddouri@users.noreply.github.com>
2024-11-14 01:12:23 +0100

    Update UniversiteService.java

--- src/main/java/tn/esprit/spring/Services/Universite/UniversiteService.java
+++ src/main/java/tn/esprit/spring/Services/Universite/UniversiteService.java
@@ -2,13 +2,7 @@ package tn.esprit.spring.Services.Universite;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-<<<<<<< HEAD
 import tn.esprit.spring.DAO.Entities.Universite;
-=======
-import tn.esprit.spring.DAO.Entities.Foyer;
-import tn.esprit.spring.DAO.Entities.Universite;
-import tn.esprit.spring.DAO.Repositories.FoyerRepository;
->>>>>>> 94e3795eee2e156635c933ee79ce07c3edc3e0cf
 import tn.esprit.spring.DAO.Repositories.UniversiteRepository;
 
 import java.util.List;
@@ -16,12 +10,8 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class UniversiteService implements IUniversiteService {
-<<<<<<< HEAD
 
     private final UniversiteRepository repo;
-=======
-    UniversiteRepository repo;
->>>>>>> 94e3795eee2e156635c933ee79ce07c3edc3e0cf
 
     @Override
     public Universite addOrUpdate(Universite u) {
@@ -35,11 +25,7 @@ public class UniversiteService implements IUniversiteService {
 
     @Override
     public Universite findById(long id) {
-<<<<<<< HEAD
         return repo.findById(id).orElse(null);
-=======
-        return repo.findById(id).get();
->>>>>>> 94e3795eee2e156635c933ee79ce07c3edc3e0cf
     }
 
     @Override
@@ -51,7 +37,6 @@ public class UniversiteService implements IUniversiteService {
     public void delete(Universite u) {
         repo.delete(u);
     }
-<<<<<<< HEAD
 
     // New functionality 1: Find universities by foyer ID and student enrollment status
     @Override
@@ -65,6 +50,3 @@ public class UniversiteService implements IUniversiteService {
         return repo.countByAdresse(adresse);
     }
 }
-=======
-}
->>>>>>> 94e3795eee2e156635c933ee79ce07c3edc3e0cf
