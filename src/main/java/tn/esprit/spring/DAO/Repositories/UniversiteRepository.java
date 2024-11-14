5b0785b20739f5030078f330dd141dfe1ca39d56
achrefmaddouri <76907474+achrefmaddouri@users.noreply.github.com>
2024-11-14 01:13:06 +0100

    Update UniversiteRepository.java

--- src/main/java/tn/esprit/spring/DAO/Repositories/UniversiteRepository.java
+++ src/main/java/tn/esprit/spring/DAO/Repositories/UniversiteRepository.java
@@ -7,7 +7,6 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface UniversiteRepository extends JpaRepository<Universite, Long> {
-<<<<<<< HEAD
 
     // Find a university by its name
     Universite findByNomUniversite(String nomUniversite);
@@ -23,11 +22,3 @@ public interface UniversiteRepository extends JpaRepository<Universite, Long> {
     // Count the number of universities located at a specific address
     long countByAdresse(String adresse);
 }
-=======
-    Universite findByNomUniversite(String nomUniversite);
-    // Afficher la liste des universités qui ont des étudiants dont leurs noms contiennet
-    // la chaine de caractère en paramètre et leurs dates de naissance entre deux dates
-    // passées en paramètre
-    List<Universite> findByFoyerBlocsChambresReservationsEtudiantsNomEtLikeAndFoyerBlocsChambresReservationsEtudiantsDateNaissanceBetween(String nom, LocalDate date1, LocalDate date2);
-}
->>>>>>> 94e3795eee2e156635c933ee79ce07c3edc3e0cf
